effcfb562d0b29893c2b0c3a9e97f38193b0b6b1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-07 22:41:53 -0400

    boards: Add pine64-pinebookPro

--- boards.nix
+++ boards.nix
@@ -70,6 +70,7 @@ in
   #
   pine64-pineA64LTS = allwinnerA64 { defconfig = "pine64-lts_defconfig"; };
   pine64-pinebookA64 = allwinnerA64 { defconfig = "pinebook_defconfig"; };
+  pine64-pinebookPro = rk3399 { defconfig = "pinebook-pro-rk3399_defconfig"; };
 
   #
   # Sandbox
