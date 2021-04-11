3684b8a58a74c7e195c0eee3827ca565ecbcd5c7
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-11 15:19:05 -0400

    allwinner-a64: Build a disk image

--- support/builders/allwinner-a64/default.nix
+++ support/builders/allwinner-a64/default.nix
@@ -1,10 +1,25 @@
-{ buildTowBoot, TF-A }:
+{ buildTowBoot, TF-A, holeyGPTDiskImageBuilder }:
 
 # For Allwinner A64 and Allwinner A64 compatible based hardware
 { defconfig, ... } @ args:
 
+let
+  offset = 8 * 1024; # in bytes... `seek * bs`
+  firmwareMaxSize = 4 * 1024 * 1024; # MiB in bytes
+  baseImage = holeyGPTDiskImageBuilder {
+    pad = (offset + firmwareMaxSize) / 512; # in sectors
+  };
+in
 buildTowBoot ({
   meta.platforms = ["aarch64-linux"];
   BL31 = "${TF-A}/bl31.bin";
   filesToInstall = ["u-boot-sunxi-with-spl.bin"];
+
+  postInstall = ''
+    echo ":: Preparing holey GPT image with embedded firmware..."
+    (PS4=" $ "; set -x
+    cat ${baseImage} > $out/disk-image.img
+    dd if=u-boot-sunxi-with-spl.bin of=$out/disk-image.img bs=1024 seek=8 conv=notrunc
+    )
+  '';
 } // args)
