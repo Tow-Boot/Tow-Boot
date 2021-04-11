c7ac40feba48fdb4312d8ae3c0f730a71b90327a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-11 01:41:31 -0400

    orangePi-zeroPlus2H5: Fix USB support

--- boards/orangePi/default.nix
+++ boards/orangePi/default.nix
@@ -1,5 +1,10 @@
 { allwinnerA64 }:
 
 {
-  orangePi-zeroPlus2H5 = allwinnerA64 { defconfig = "orangepi_zero_plus2_defconfig"; };
+  orangePi-zeroPlus2H5 = allwinnerA64 {
+    defconfig = "orangepi_zero_plus2_defconfig";
+    patches = [
+      ./0001-sun50i-h5-orangepi-zero-plus2-Enable-USB.patch
+    ];
+  };
 }
