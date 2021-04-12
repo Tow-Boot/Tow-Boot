541e065a0b93e66a1425819cd42c7b87f138e823
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-11 22:07:05 -0400

    pine64-pinebookPro: Add display patchset
    It's accepted for the next release:
    
    > Series applied to u-boot-video/master, thanks!

--- boards/pine64/default.nix
+++ boards/pine64/default.nix
@@ -1,7 +1,15 @@
-{ allwinnerA64, rockchipRK399 }:
+{ allwinnerA64, rockchipRK399, fetchpatch }:
 
 {
   pine64-pineA64LTS = allwinnerA64 { defconfig = "pine64-lts_defconfig"; };
   pine64-pinebookA64 = allwinnerA64 { defconfig = "pinebook_defconfig"; };
-  pine64-pinebookPro = rockchipRK399 { defconfig = "pinebook-pro-rk3399_defconfig"; };
+  pine64-pinebookPro = rockchipRK399 {
+    defconfig = "pinebook-pro-rk3399_defconfig";
+    patches = [
+      (fetchpatch {
+        url = "https://patchwork.ozlabs.org/series/232334/mbox/";
+        sha256 = "0abmc82dccwmf8fjg7lyxx33r0dfc9h2hrx5d32sjl6mfj88hkj7";
+      })
+    ];
+  };
 }
