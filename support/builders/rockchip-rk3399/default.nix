87ae38d77af1f9d42c10ceec3c687f2832b129b0
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-14 19:17:26 -0400

    rockchip-rk3399: Rework to work from discrete derivations
    Ending up with a simple runCommand that assembles what we want.

--- support/builders/rockchip-rk3399/default.nix
+++ support/builders/rockchip-rk3399/default.nix
@@ -1,4 +1,4 @@
-{ buildTowBoot, TF-A, GPTDiskImageBuilder }:
+{ buildTowBoot, TF-A, GPTDiskImageBuilder, runCommandNoCC }:
 
 # For Rockchip RK3399 based hardware
 { defconfig, postPatch ? "", postInstall ? "", ... } @ args:
@@ -10,58 +10,69 @@ let
   secondOffset = 16384; # in sectors
   sectorSize = 512;
   baseImage = GPTDiskImageBuilder {
+    name = "disk-image";
+    inherit sectorSize;
     partitionOffset = partitionOffset; # in sectors
     partitionSize = firmwareMaxSize + (secondOffset * sectorSize); # in bytes
+    firmwareFile = "${firmware}/firmware.shared.img";
   };
-in
-buildTowBoot ({
-  # Does not actually turn off tested boards...
-  withPoweroff = false;
 
-  inherit defconfig;
-  inherit
-    sectorSize
-    partitionOffset
-    secondOffset
-  ;
+  firmware = buildTowBoot ({
+    # Does not actually turn off tested boards...
+    withPoweroff = false;
 
-  meta.platforms = ["aarch64-linux"];
-  BL31 = "${TF-A}/bl31.elf";
+    inherit defconfig;
+    inherit
+      sectorSize
+      partitionOffset
+      secondOffset
+    ;
 
-  postPatch = ''
-    patchShebangs arch/arm/mach-rockchip/
-  '' + postPatch;
+    meta.platforms = ["aarch64-linux"];
+    BL31 = "${TF-A}/bl31.elf";
 
-  postInstall = ''
-    echo ":: Preparing image for SPI flash..."
-    (PS4=" $ "; set -x
-    tools/mkimage -n rk3399 -T rkspi -d tpl/u-boot-tpl-dtb.bin:spl/u-boot-spl-dtb.bin spl.bin
-    cat <(dd if=spl.bin bs=512K conv=sync) u-boot.itb > $out/firmware.spiflash.bin
-    )
+    postPatch = ''
+      patchShebangs arch/arm/mach-rockchip/
+    '' + postPatch;
 
-    echo ":: Preparing single file firmware image for shared storage..."
-    (PS4=" $ "; set -x
-    dd if=idbloader.img of=firmware.shared.img conv=fsync,notrunc bs=$sectorSize seek=$((partitionOffset - partitionOffset))
-    dd if=u-boot.itb    of=firmware.shared.img conv=fsync,notrunc bs=$sectorSize seek=$((secondOffset - partitionOffset))
-    cp -v firmware.shared.img $out/
-    )
+    postInstall = ''
+      echo ":: Preparing image for SPI flash..."
+      (PS4=" $ "; set -x
+      tools/mkimage -n rk3399 -T rkspi -d tpl/u-boot-tpl-dtb.bin:spl/u-boot-spl-dtb.bin spl.bin
+      cat <(dd if=spl.bin bs=512K conv=sync) u-boot.itb > $out/firmware.spiflash.bin
+      )
 
-    echo ":: Preparing GPT image with embedded firmware..."
-    (PS4=" $ "; set -x
-    cat ${baseImage} > $out/disk-image.img
-    dd if=firmware.shared.img of=$out/disk-image.img bs=512 seek=$partitionOffset conv=notrunc
-    )
-  '' + postInstall;
+      echo ":: Preparing single file firmware image for shared storage..."
+      (PS4=" $ "; set -x
+      dd if=idbloader.img of=firmware.shared.img conv=fsync,notrunc bs=$sectorSize seek=$((partitionOffset - partitionOffset))
+      dd if=u-boot.itb    of=firmware.shared.img conv=fsync,notrunc bs=$sectorSize seek=$((secondOffset - partitionOffset))
+      cp -v firmware.shared.img $out/
+      )
+    '' + postInstall;
 
-  extraConfig = ''
-    # SPI boot Support
-    CONFIG_MTD=y
-    CONFIG_DM_MTD=y
-    CONFIG_SPI_FLASH_SFDP_SUPPORT=y
-    CONFIG_SPL_DM_SPI=y
-    CONFIG_SPL_SPI_FLASH_TINY=n
-    CONFIG_SPL_SPI_FLASH_SFDP_SUPPORT=y
-    CONFIG_SYS_SPI_U_BOOT_OFFS=0x80000
-    CONFIG_SPL_DM_SEQ_ALIAS=y
+    extraConfig = ''
+      # SPI boot Support
+      CONFIG_MTD=y
+      CONFIG_DM_MTD=y
+      CONFIG_SPI_FLASH_SFDP_SUPPORT=y
+      CONFIG_SPL_DM_SPI=y
+      CONFIG_SPL_SPI_FLASH_TINY=n
+      CONFIG_SPL_SPI_FLASH_SFDP_SUPPORT=y
+      CONFIG_SYS_SPI_U_BOOT_OFFS=0x80000
+      CONFIG_SPL_DM_SEQ_ALIAS=y
+    '';
+  } // removeAttrs args [ "postPatch" ]);
+
+  output = runCommandNoCC firmware.name {
+    inherit
+      sectorSize
+      partitionOffset
+      secondOffset
+    ;
+  } ''
+    mkdir -p "$out"
+    cp -rvt $out/ ${firmware}/*
+    cp -rvt $out/ ${baseImage}/*
   '';
-} // removeAttrs args [ "postPatch" ])
+in
+  output
