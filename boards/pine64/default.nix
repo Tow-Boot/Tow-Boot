008f963105b5c5fdcce7e3f0250f2258a365e09c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-13 20:10:12 -0400

    pine64-pinebookPro: Add LEDs support

--- boards/pine64/default.nix
+++ boards/pine64/default.nix
@@ -5,7 +5,16 @@
   pine64-pinebookA64 = allwinnerA64 { defconfig = "pinebook_defconfig"; };
   pine64-pinebookPro = rockchipRK399 {
     defconfig = "pinebook-pro-rk3399_defconfig";
+    postPatch =
+      let
+        setup_leds = "led green:power on; led red:standby on";
+      in
+    ''
+      substituteInPlace include/tow-boot_env.h \
+        --replace 'setup_leds=\0' 'setup_leds=${setup_leds}\0'
+    '';
     patches = [
+      ./0001-rk3399-light-pinebook-power-and-standby-leds-during-.patch
       (fetchpatch {
         url = "https://patchwork.ozlabs.org/series/232334/mbox/";
         sha256 = "0abmc82dccwmf8fjg7lyxx33r0dfc9h2hrx5d32sjl6mfj88hkj7";
