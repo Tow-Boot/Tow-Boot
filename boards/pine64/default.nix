945527fc12dfe168487bdb110ceffb92bee6e66a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-15 22:56:19 -0400

    pine64-pinebookPro: Add suspend resume patch
    Note that this also splits the build into its own file because it
    started being a bit bigger than expected!

--- boards/pine64/default.nix
+++ boards/pine64/default.nix
@@ -1,4 +1,4 @@
-{ allwinnerA64, rockchipRK399, fetchpatch }:
+{ Tow-Boot, allwinnerA64, rockchipRK399 }:
 
 {
   pine64-pineA64LTS = allwinnerA64 {
@@ -9,25 +9,7 @@
     ];
   };
   pine64-pinebookA64 = allwinnerA64 { defconfig = "pinebook_defconfig"; };
-  pine64-pinebookPro = rockchipRK399 {
-    defconfig = "pinebook-pro-rk3399_defconfig";
-    postPatch =
-      let
-        setup_leds = "led green:power on; led red:standby on";
-      in
-    ''
-      substituteInPlace include/tow-boot_env.h \
-        --replace 'setup_leds=\0' 'setup_leds=${setup_leds}\0'
-    '';
-    patches = [
-      ./0001-rk3399-light-pinebook-power-and-standby-leds-during-.patch
-      ./0001-rk3399-pinebook-pro-Support-SPI-flash-boot.patch
-      (fetchpatch {
-        url = "https://patchwork.ozlabs.org/series/232334/mbox/";
-        sha256 = "0abmc82dccwmf8fjg7lyxx33r0dfc9h2hrx5d32sjl6mfj88hkj7";
-      })
-    ];
-  };
+  pine64-pinebookPro = Tow-Boot.systems.aarch64.callPackage ./pinebook-pro.nix { };
   pine64-rockpro64 = rockchipRK399 {
     defconfig = "rockpro64-rk3399_defconfig";
     patches = [
---
+++ boards/pine64/pinebook-pro.nix
@@ -0,0 +1,32 @@
+{ rockchipRK399, fetchpatch, armTrustedFirmwareRK3399 }:
+
+let
+  TF-A = armTrustedFirmwareRK3399.overrideAttrs({ patches ? [ ], ...}: {
+    patches = [
+      (fetchpatch {
+        url = "https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git/patch/?id=1738a04b588200d3e737382da07e4f967305b4f2";
+        sha256 = "09rmwa9568wac9y8xf0jvnmhdga7j2hp02gk4451w0v2is3sfchg";
+      })
+    ] ++ patches;
+  });
+in
+rockchipRK399 {
+  defconfig = "pinebook-pro-rk3399_defconfig";
+  postPatch =
+    let
+      setup_leds = "led green:power on; led red:standby on";
+    in
+  ''
+    substituteInPlace include/tow-boot_env.h \
+      --replace 'setup_leds=\0' 'setup_leds=${setup_leds}\0'
+  '';
+  patches = [
+    ./0001-rk3399-light-pinebook-power-and-standby-leds-during-.patch
+    ./0001-rk3399-pinebook-pro-Support-SPI-flash-boot.patch
+    (fetchpatch {
+      url = "https://patchwork.ozlabs.org/series/232334/mbox/";
+      sha256 = "0abmc82dccwmf8fjg7lyxx33r0dfc9h2hrx5d32sjl6mfj88hkj7";
+    })
+  ];
+  BL31 = "${TF-A}/bl31.elf";
+}
