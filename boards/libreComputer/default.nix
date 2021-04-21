383ad62f2d931ea33883956ed033d85e3c6faf2e
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-20 23:29:01 -0400

    boards: Add libreComputer-rocRk3399Pc + Mezzanine

--- boards/libreComputer/default.nix
+++ boards/libreComputer/default.nix
@@ -1,9 +1,21 @@
-{ amlogicFirmware, amlogicGXL }:
+{ amlogicFirmware, amlogicGXL, rockchipRK399 }:
 
 {
   libreComputer-amlS805xAc = amlogicGXL {
     defconfig = "libretech-ac_defconfig";
     FIPDIR = "${amlogicFirmware}/lafrite";
     withSPI = true;
+  };
+  libreComputer-rocRk3399Pc = rockchipRK399 {
+    defconfig = "roc-pc-rk3399_defconfig";
+    patches = [
+      ./0001-rk3399-roc-pc-Configure-SPI-flash-boot-offset.patch
+    ];
+  };
+  libreComputer-rocRk3399PcMezzanine = rockchipRK399 {
+    defconfig = "roc-pc-mezzanine-rk3399_defconfig";
+    patches = [
+      ./0001-rk3399-roc-pc-Configure-SPI-flash-boot-offset.patch
+    ];
   };
 }
