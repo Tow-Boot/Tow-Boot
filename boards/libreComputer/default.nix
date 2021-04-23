a3e94a72e2f11bc0d84fc346bd447f09db5ca79d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-22 21:54:24 -0400

    libreComputer-rocRk3399Pc*: Review build

--- boards/libreComputer/default.nix
+++ boards/libreComputer/default.nix
@@ -1,21 +1,33 @@
 { amlogicFirmware, amlogicGXL, rockchipRK399 }:
 
+let
+  # Re-used for two boards.
+  rocRk3399Pc = { defconfig }: rockchipRK399 {
+    inherit defconfig;
+    patches = [
+      ./0001-rk3399-roc-pc-Configure-SPI-flash-boot-offset.patch
+      ./roc-pc-config.patch
+    ];
+    postPatch =
+      let
+        setup_leds = "led green:work on; led red:diy on";
+      in
+    ''
+      substituteInPlace include/tow-boot_env.h \
+        --replace 'setup_leds=\0' 'setup_leds=${setup_leds}\0'
+    '';
+  };
+in
 {
   libreComputer-amlS805xAc = amlogicGXL {
     defconfig = "libretech-ac_defconfig";
     FIPDIR = "${amlogicFirmware}/lafrite";
     withSPI = true;
   };
-  libreComputer-rocRk3399Pc = rockchipRK399 {
+  libreComputer-rocRk3399Pc = rocRk3399Pc {
     defconfig = "roc-pc-rk3399_defconfig";
-    patches = [
-      ./0001-rk3399-roc-pc-Configure-SPI-flash-boot-offset.patch
-    ];
   };
-  libreComputer-rocRk3399PcMezzanine = rockchipRK399 {
+  libreComputer-rocRk3399PcMezzanine = rocRk3399Pc {
     defconfig = "roc-pc-mezzanine-rk3399_defconfig";
-    patches = [
-      ./0001-rk3399-roc-pc-Configure-SPI-flash-boot-offset.patch
-    ];
   };
 }
