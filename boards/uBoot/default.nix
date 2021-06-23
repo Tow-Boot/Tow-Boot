10cbac81d93d3e0b2d433f1d0efc00728926eb0a
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-06-22 21:06:55 -0400

    uBoot-*: Implement variant and name changes

--- boards/uBoot/default.nix
+++ boards/uBoot/default.nix
@@ -14,7 +14,6 @@ in
 
   uBoot-sandbox = buildTowBoot {
     defconfig = "sandbox_defconfig";
-    filesToInstall = ["u-boot" "u-boot.dtb"];
     buildInputs = with nixpkgs; [
       SDL2
       perl
@@ -23,6 +22,15 @@ in
       ./0001-sandbox-Force-window-size.patch
     ];
     internal = true;
+    variant = "noenv";
+
+    # TODO: Add helper bin to start with dtb file
+    installPhase = ''
+      rmdir $out/binaries
+      mkdir -p $out/libexec
+      cp -v u-boot $out/libexec/tow-boot
+      cp -v u-boot.dtb $out/tow-boot.dtb
+    '';
   };
 
   # Virtualization targets
@@ -30,29 +38,37 @@ in
   uBoot-qemuArm = armv7l.buildTowBoot {
     defconfig = "qemu_arm_defconfig";
     meta.platforms = ["armv7l-linux"];
-    filesToInstall = ["u-boot.bin"];
     internal = true;
+    variant = "noenv";
+    installPhase = ''
+      cp -v u-boot.bin $out/binaries/tow-boot.$variant.bin
+    '';
   };
 
   uBoot-qemuArm64 = aarch64.buildTowBoot {
     defconfig = "qemu_arm64_defconfig";
     meta.platforms = ["aarch64-linux"];
-    filesToInstall = ["u-boot.bin"];
     internal = true;
+    variant = "noenv";
+    installPhase = ''
+      cp -v u-boot.bin $out/binaries/tow-boot.$variant.bin
+    '';
   };
 
   uBoot-qemuX86 = i686.buildTowBoot {
     defconfig = "qemu-x86_defconfig";
     meta.platforms = ["i686-linux"];
-    filesToInstall = ["u-boot.rom"];
     withPoweroff = false;
     internal = true;
+    variant = "noenv";
+    installPhase = ''
+      cp -v u-boot.rom $out/binaries/tow-boot.$variant.rom
+    '';
   };
 
   uBoot-qemuX86_64 = x86_64.buildTowBoot {
     defconfig = "qemu-x86_64_defconfig";
     meta.platforms = ["x86_64-linux"];
-    filesToInstall = ["u-boot.rom"];
 
     # Enabling the logo breaks things left and right
     withLogo = false;
@@ -62,6 +78,13 @@ in
     withTTF = false;
     withPoweroff = false;
     internal = true;
+    variant = "noenv";
+    installPhase = ''
+      cp -v u-boot.rom $out/binaries/tow-boot.$variant.rom
+    '';
+    extraConfig = ''
+      CONFIG_SPL_ENV_SUPPORT=y
+    '';
   };
 
   # EFI payloads
@@ -69,16 +92,22 @@ in
   uBoot-efiX86 = i686.buildTowBoot {
     defconfig = "efi-x86_payload32_defconfig";
     meta.platforms = ["i686-linux"];
-    filesToInstall = ["u-boot-payload.efi"];
     withPoweroff = false;
     internal = true;
+    variant = "noenv";
+    installPhase = ''
+      cp -v u-boot-payload.efi $out/binaries/tow-boot.$variant.efi
+    '';
   };
 
   uBoot-efiX86_64 = x86_64.buildTowBoot {
     defconfig = "efi-x86_payload64_defconfig";
     meta.platforms = ["x86_64-linux"];
-    filesToInstall = ["u-boot-payload.efi"];
     withPoweroff = false;
     internal = true;
+    variant = "noenv";
+    installPhase = ''
+      cp -v u-boot-payload.efi $out/binaries/tow-boot.$variant.efi
+    '';
   };
 }
