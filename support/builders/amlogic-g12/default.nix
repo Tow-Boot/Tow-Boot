bf1dc4215001cb0e24d6116955e0e78aa46efb73
Las Safin <me@las.rs>
2021-11-11 11:47:55 +0000

    Fix using keyboard in SPI on G12 (Thanks @tpwrules!)

--- support/builders/amlogic-g12/default.nix
+++ support/builders/amlogic-g12/default.nix
@@ -1,7 +1,7 @@
 { lib, buildTowBoot, meson64-tools, imageBuilder, runCommandNoCC, spiInstallerPartitionBuilder }:
 
 let self =
-{ FIPDIR, withSPI ? false,  ...} @ args':
+{ FIPDIR, withSPI ? false, extraConfig ? "", ...} @ args':
 
 let
   args = removeAttrs args' (builtins.attrNames (builtins.functionArgs self));
@@ -78,6 +78,11 @@ let
     installPhase = ''
       cp -v Tow-Boot.bin $out/binaries/Tow-Boot.$variant.bin
     '';
+
+    extraConfig = ''
+      CONFIG_USE_PREBOOT=y
+    '' + extraConfig;
+
   } // args);
 
   firmware = firmware' "noenv";
