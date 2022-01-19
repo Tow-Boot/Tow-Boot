b71ca85fd1b56b5b9ecb78766f4fdd77a548f551
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-18 20:07:39 -0500

    pine64-pinebookPro: Remove now mainlined TF-A patch

--- boards/pine64/pinebook-pro.nix
+++ boards/pine64/pinebook-pro.nix
@@ -1,15 +1,5 @@
-{ rockchipRK399, fetchpatch, armTrustedFirmwareRK3399 }:
+{ rockchipRK399, fetchpatch }:
 
-let
-  TF-A = armTrustedFirmwareRK3399.overrideAttrs({ patches ? [ ], ...}: {
-    patches = [
-      (fetchpatch {
-        url = "https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git/patch/?id=1738a04b588200d3e737382da07e4f967305b4f2";
-        sha256 = "09rmwa9568wac9y8xf0jvnmhdga7j2hp02gk4451w0v2is3sfchg";
-      })
-    ] ++ patches;
-  });
-in
 rockchipRK399 {
   boardIdentifier = "pine64-pinebookPro";
   defconfig = "pinebook-pro-rk3399_defconfig";
@@ -32,5 +22,4 @@ rockchipRK399 {
       sha256 = "0aiw9zk8w4msd3v8nndhkspjify0yq6a5f0zdy6mhzs0ilq896c3";
     })
   ];
-  BL31 = "${TF-A}/bl31.elf";
 }
