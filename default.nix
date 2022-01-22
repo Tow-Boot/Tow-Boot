f6f0fe9c43143c73c0ebff6f461740d9cd23cee8
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-21 21:55:09 -0500

    default.nix: Enable automatic cross-compilation

--- default.nix
+++ default.nix
@@ -14,6 +14,7 @@ import ./support/nix/eval-with-configuration.nix (args // {
         { lib, ... }:
         {
           # Special configs for imperative use only here
+          system.automaticCross = true;
         }
       )
     ];
