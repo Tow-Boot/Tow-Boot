28cd23acbd451adb78305d9fb3eb3722f5cb0dde
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-21 21:17:11 -0500

    boards: Partly migrate olimex boards

---
+++ boards/olimex-teresI/default.nix
@@ -0,0 +1,15 @@
+{
+  device = {
+    manufacturer = "Olimex";
+    name = "TERES-I";
+    identifier = "olimex-teresI";
+  };
+
+  hardware = {
+    soc = "allwinner-a64";
+  };
+
+  Tow-Boot = {
+    defconfig = "teres_i_defconfig";
+  };
+}
