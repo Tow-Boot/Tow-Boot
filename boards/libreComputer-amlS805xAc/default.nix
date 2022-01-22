fb7ac7537b329a1954032dd4454a780eb0888516
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-21 21:16:27 -0500

    boards: Partly migrate libreComputer boards

---
+++ boards/libreComputer-amlS805xAc/default.nix
@@ -0,0 +1,24 @@
+{ lib, pkgs, ... }:
+
+{
+  device = {
+    manufacturer = "Libre Computer";
+    name = "La Frite";
+    identifier = "libreComputer-amlS805xAc";
+  };
+
+  hardware = {
+    soc = "amlogic-s805x";
+    SPISize = 128 /* Mbits */ * 1024 * 1024 / 8; # equiv to 16 MiB
+  };
+
+  Tow-Boot = {
+    defconfig = "libretech-ac_defconfig";
+  };
+
+  TEMP = {
+    legacyBuilderArguments = {
+      FIPDIR = "${pkgs.Tow-Boot.amlogicFirmware}/lafrite";
+    };
+  };
+}
