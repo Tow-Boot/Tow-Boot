2ab470244a5c3bbabf6fb04c904fcd4fe96913fa
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-24 15:16:08 -0500

    hardware/rockchip: Use the new builder

--- modules/hardware/rockchip/default.nix
+++ modules/hardware/rockchip/default.nix
@@ -1,11 +1,17 @@
 { config, lib, pkgs, ... }:
 
 let
+  inherit (pkgs)
+    fetchpatch
+  ;
   inherit (lib)
     mkIf
     mkMerge
     mkOption
     types
+  ;
+  inherit (config.Tow-Boot)
+    variant
   ;
   cfg = config.hardware.socs;
 in
@@ -29,9 +35,80 @@ in
     }
     (mkIf cfg.rockchip-rk3399.enable {
       system.system = "aarch64-linux";
-      # XXX legacy builder support
-      TEMP = {
-        legacyBuilder = pkgs.Tow-Boot.rockchipRK3399;
+      Tow-Boot = {
+        config = [
+          (helpers: with helpers; {
+            # SPI boot Support
+            MTD = yes;
+            DM_MTD = yes;
+            SPI_FLASH_SFDP_SUPPORT = yes;
+            SPL_DM_SPI = yes;
+            SPL_SPI_FLASH_TINY = no;
+            SPL_SPI_FLASH_SFDP_SUPPORT = yes;
+            SYS_SPI_U_BOOT_OFFS = freeform ''0x80000''; # 512K
+            SPL_DM_SEQ_ALIAS = yes;
+
+            # Not supported on this platform
+            CMD_POWEROFF = no;
+          })
+        ];
+        patches = [
+          ./0001-HACK-efi_runtime-pretend-we-can-t-reset.patch
+          #
+          # eMMC woes fixes
+          # ---------------
+          #
+          # Ensures eMMC nodes are present in SPL FDT.
+          #
+          (fetchpatch {
+            url = "https://source.denx.de/u-boot/u-boot/-/commit/f8b36089af26c3596a8b3796af336cee42cc1757.patch";
+            sha256 = "sha256-iFqR5CJ/X0Fz41Ta3kcZW48Kcm9mI4m5bAcp66PkNU0=";
+          })
+          #
+          # Unbreaks eMMC for some usage.
+          # First two patches are changes needed to apply the series.
+          #
+          (fetchpatch {
+            url = "https://source.denx.de/u-boot/u-boot/-/commit/40e6f52454fc9adb6269ef8089c1fd2ded85fee8.patch";
+            sha256 = "sha256-RGBfAR8YC3kY3/2C4cFQR59DtMvYDUdwE6++0jGPNi0=";
+          })
+          (fetchpatch {
+            url = "https://source.denx.de/u-boot/u-boot/-/commit/022f552704b6467966e4fad39c85a6aca9204c94.patch";
+            sha256 = "sha256-mDWlJQQjQykb9kzIKZYEBI2Ktdpgc7LZyWspvb2F62w=";
+          })
+          (fetchpatch {
+            # https://patchwork.ozlabs.org/project/uboot/cover/20220116201814.11672-1-alpernebiyasak@gmail.com/
+            url = "https://patchwork.ozlabs.org/series/281327/mbox/";
+            sha256 = "sha256-gjHwZWIPUzWMUk2+7Mhd4XJuorBluVL9J9LaO9fUaKw=";
+          })
+        ];
+        builder = {
+          additionalArguments = {
+            BL31 = "${pkgs.Tow-Boot.armTrustedFirmwareRK3399}/bl31.elf";
+            firmwareMaxSize = 4 * 1024 * 1024; # MiB in bytes
+            partitionOffset = 64; # in sectors
+            secondOffset = 16384; # in sectors
+            sectorSize = 512;
+          };
+          installPhase = mkMerge [
+            (mkIf (variant == "spi") ''
+              echo ":: Preparing image for SPI flash..."
+              (PS4=" $ "; set -x
+              tools/mkimage -n rk3399 -T rkspi -d tpl/u-boot-tpl-dtb.bin:spl/u-boot-spl-dtb.bin spl.bin
+              # 512K here is 0x80000 CONFIG_SYS_SPI_U_BOOT_OFFS
+              cat <(dd if=spl.bin bs=512K conv=sync) u-boot.itb > $out/binaries/Tow-Boot.$variant.bin
+              )
+            '')
+            (mkIf (variant != "spi") ''
+              echo ":: Preparing single file firmware image for shared storage..."
+              (PS4=" $ "; set -x
+              dd if=idbloader.img of=Tow-Boot.$variant.bin conv=fsync,notrunc bs=$sectorSize seek=$((partitionOffset - partitionOffset))
+              dd if=u-boot.itb    of=Tow-Boot.$variant.bin conv=fsync,notrunc bs=$sectorSize seek=$((secondOffset - partitionOffset))
+              cp -v Tow-Boot.$variant.bin $out/binaries/
+              )
+            '')
+          ];
+        };
       };
     })
   ];
