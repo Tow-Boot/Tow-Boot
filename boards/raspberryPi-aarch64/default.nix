3731782a7a9e275c32d45805c993e65295a53901
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-24 17:39:59 -0500

    raspberryPi-aarch64: Implement build using the new builder

--- boards/raspberryPi-aarch64/default.nix
+++ boards/raspberryPi-aarch64/default.nix
@@ -1,10 +1,27 @@
-{ lib, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  inherit (config.helpers)
+    composeConfig
+  ;
+  raspberryPi-3 = composeConfig {
+    config = {
+      device.identifier = "raspberryPi-3";
+      Tow-Boot.defconfig = "rpi_3_defconfig";
+    };
+  };
+  raspberryPi-4 = composeConfig {
+    config = {
+      device.identifier = "raspberryPi-4";
+      Tow-Boot.defconfig = "rpi_4_defconfig";
+    };
+  };
+in
 {
   device = {
     manufacturer = "Raspberry Pi";
     name = "Combined AArch64";
-    identifier = "raspberryPi-aarch64";
+    identifier = lib.mkDefault "raspberryPi-aarch64";
   };
 
   hardware = {
@@ -12,10 +29,36 @@
     soc = "generic-aarch64";
   };
 
-  # Workaround for the builder being required even if actually unused.
-  TEMP.legacyBuilder = _: null;
+  Tow-Boot = {
+    config = [
+      (helpers: with helpers; {
+        CMD_POWEROFF = no;
+      })
+    ];
+    patches = [
+      ./0001-configs-rpi-allow-for-bigger-kernels.patch
+      ./0001-Tow-Boot-rpi-Increase-malloc-pool-up-to-64MiB-env.patch
+    ];
+    firmwareBuild = lib.mkIf (config.device.identifier == "raspberryPi-aarch64") (
+      pkgs.callPackage (
+        { runCommandNoCC }:
 
-  # Directly call the builder
-  # This build is not like any others.
-  build.default = lib.mkForce (pkgs.Tow-Boot.callPackage ./builder.nix { });
+        runCommandNoCC "tow-boot-${config.device.identifier}" { } ''
+          (PS4=" $ "; set -x
+          mkdir -p $out/{binaries,config}
+          cp -v ${raspberryPi-3.config.Tow-Boot.firmwareBuild.source}/* $out/
+          cp -v ${raspberryPi-3.config.Tow-Boot.firmwareBuild}/binaries/Tow-Boot.noenv.bin $out/binaries/Tow-Boot.noenv.rpi3.bin
+          cp -v ${raspberryPi-3.config.Tow-Boot.firmwareBuild}/config/noenv.config $out/config/noenv.rpi3.config
+
+          cp -v ${raspberryPi-4.config.Tow-Boot.firmwareBuild.source}/* $out/
+          cp -v ${raspberryPi-4.config.Tow-Boot.firmwareBuild}/binaries/Tow-Boot.noenv.bin $out/binaries/Tow-Boot.noenv.rpi4.bin
+          cp -v ${raspberryPi-4.config.Tow-Boot.firmwareBuild}/config/noenv.config $out/config/noenv.rpi4.config
+          )
+        ''
+      ) { }
+    );
+    builder.installPhase = ''
+      cp -v u-boot.bin $out/binaries/Tow-Boot.$variant.bin
+    '';
+  };
 }
