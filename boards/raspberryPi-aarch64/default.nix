2582cf7f417c839877f254ddac0c37fb4c398620
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-27 01:28:22 -0500

    raspberryPi-aarch64: Produce disk image

--- boards/raspberryPi-aarch64/default.nix
+++ boards/raspberryPi-aarch64/default.nix
@@ -16,6 +16,22 @@ let
       Tow-Boot.defconfig = "rpi_4_defconfig";
     };
   };
+
+  configTxt = pkgs.writeText "config.txt" ''
+    [pi3]
+    kernel=Tow-Boot.noenv.rpi3.bin
+
+    [pi4]
+    kernel=Tow-Boot.noenv.rpi4.bin
+    enable_gic=1
+    armstub=armstub8-gic.bin
+    disable_overscan=1
+
+    [all]
+    arm_64bit=1
+    enable_uart=1
+    avoid_warnings=1
+  '';
 in
 {
   device = {
@@ -60,5 +76,38 @@ in
     builder.installPhase = ''
       cp -v u-boot.bin $out/binaries/Tow-Boot.$variant.bin
     '';
+
+    # The Raspberry Pi firmware expects a filesystem to be used.
+    writeBinaryToFirmwarePartition = false;
+
+    diskImage = {
+      partitioningScheme = "mbr";
+    };
+    firmwarePartition = {
+      partitionType = "0C";
+      filesystem = {
+        filesystem = "fat32";
+        populateCommands = ''
+          cp -v ${configTxt} config.txt
+          cp -v ${raspberryPi-3.config.Tow-Boot.outputs.firmware}/binaries/Tow-Boot.noenv.bin Tow-Boot.noenv.rpi3.bin
+          cp -v ${raspberryPi-4.config.Tow-Boot.outputs.firmware}/binaries/Tow-Boot.noenv.bin Tow-Boot.noenv.rpi4.bin
+          cp -v ${pkgs.raspberrypi-armstubs}/armstub8-gic.bin armstub8-gic.bin
+          (
+          target="$PWD"
+          cd ${pkgs.raspberrypifw}/share/raspberrypi/boot
+          cp -v bcm2711-rpi-4-b.dtb "$target/"
+          cp -v bootcode.bin fixup*.dat start*.elf "$target/"
+          )
+        '';
+
+        # The build, since it includes misc. files from the Raspberry Pi Foundation
+        # can get quite bigger, compared to other boards.
+        size = 32 * 1024 * 1024;
+        fat32 = {
+          partitionID = "00F800F8";
+        };
+        label = "TOW-BOOT-FIRM";
+      };
+    };
   };
 }
