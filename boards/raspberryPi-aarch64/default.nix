28a8117be9267155ac2afcf2990bdd149638b969
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-27 00:32:40 -0500

    raspberryPi-aarch64: Use outputs.firmware

--- boards/raspberryPi-aarch64/default.nix
+++ boards/raspberryPi-aarch64/default.nix
@@ -39,20 +39,20 @@ in
       ./0001-configs-rpi-allow-for-bigger-kernels.patch
       ./0001-Tow-Boot-rpi-Increase-malloc-pool-up-to-64MiB-env.patch
     ];
-    firmwareBuild = lib.mkIf (config.device.identifier == "raspberryPi-aarch64") (
+    outputs.firmware = lib.mkIf (config.device.identifier == "raspberryPi-aarch64") (
       pkgs.callPackage (
         { runCommandNoCC }:
 
         runCommandNoCC "tow-boot-${config.device.identifier}" { } ''
           (PS4=" $ "; set -x
           mkdir -p $out/{binaries,config}
-          cp -v ${raspberryPi-3.config.Tow-Boot.firmwareBuild.source}/* $out/
-          cp -v ${raspberryPi-3.config.Tow-Boot.firmwareBuild}/binaries/Tow-Boot.noenv.bin $out/binaries/Tow-Boot.noenv.rpi3.bin
-          cp -v ${raspberryPi-3.config.Tow-Boot.firmwareBuild}/config/noenv.config $out/config/noenv.rpi3.config
+          cp -v ${raspberryPi-3.config.Tow-Boot.outputs.firmware.source}/* $out/
+          cp -v ${raspberryPi-3.config.Tow-Boot.outputs.firmware}/binaries/Tow-Boot.noenv.bin $out/binaries/Tow-Boot.noenv.rpi3.bin
+          cp -v ${raspberryPi-3.config.Tow-Boot.outputs.firmware}/config/noenv.config $out/config/noenv.rpi3.config
 
-          cp -v ${raspberryPi-4.config.Tow-Boot.firmwareBuild.source}/* $out/
-          cp -v ${raspberryPi-4.config.Tow-Boot.firmwareBuild}/binaries/Tow-Boot.noenv.bin $out/binaries/Tow-Boot.noenv.rpi4.bin
-          cp -v ${raspberryPi-4.config.Tow-Boot.firmwareBuild}/config/noenv.config $out/config/noenv.rpi4.config
+          cp -v ${raspberryPi-4.config.Tow-Boot.outputs.firmware.source}/* $out/
+          cp -v ${raspberryPi-4.config.Tow-Boot.outputs.firmware}/binaries/Tow-Boot.noenv.bin $out/binaries/Tow-Boot.noenv.rpi4.bin
+          cp -v ${raspberryPi-4.config.Tow-Boot.outputs.firmware}/config/noenv.config $out/config/noenv.rpi4.config
           )
         ''
       ) { }
