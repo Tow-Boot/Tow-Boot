31b1ac1eeace78176f24c6bc69e2b7571ad2635f
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-01-29 01:26:23 -0500

    tow-boot/builder: Make build more reproducible
    The source archive will be compressed in the actual release archive.
    
    And the fixed date makes the output reproducible. Helpful when fixing
    huge changes.

--- modules/tow-boot/builder.nix
+++ modules/tow-boot/builder.nix
@@ -147,8 +147,12 @@ in
             archive="Tow-Boot.$boardIdentifier.$version.$variant.src"
             mkdir -p $source
             tar \
-              -acf \
-              "$source/$archive.tar.gz" \
+              --sort=name \
+              --mtime="@''${SOURCE_DATE_EPOCH}" \
+              --owner=0 --group=0 --numeric-owner \
+              --pax-option=exthdr.name=%d/PaxHeaders/%f,delete=atime,delete=ctime \
+              -cf \
+              "$source/$archive.tar" \
               . \
               --transform "s,^,$archive/,"
             )
