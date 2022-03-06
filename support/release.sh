201ae1433dd937e55fed1bf67725ee23eac32ac0
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-03-05 22:02:27 -0500

    release.sh: Update for latest changes

--- support/release.sh
+++ support/release.sh
@@ -15,26 +15,30 @@ set -x
 
 # Remove the `-pre` suffix
 sed -i -e \
-	's/additionalIdentifier = "-pre";/additionalIdentifier = "";/' \
-	support/builders/tow-boot/default.nix
+	's/releaseIdentifier = "-pre";/releaseIdentifier = "";/' \
+	modules/tow-boot/identity.nix
 
 # Commit and tag the version
 version="$(nix-instantiate --eval -E '(import ./release.nix {}).version' | sed -e 's/"//g')"
-git add support/builders/tow-boot/default.nix
+git add modules/tow-boot/identity.nix
 git commit -m "release $version"
 git tag "release-$version"
+
+cat modules/tow-boot/identity.nix
 
 # Increment the Tow-Boot version identifier
 # (https://stackoverflow.com/a/32841608)
 sed -i -e \
-	's/towBootIdentifier = "[0-9]*[0-9]/&¤/g;:a {s/0¤/1/g;s/1¤/2/g;s/2¤/3/g;s/3¤/4/g;s/4¤/5/g;s/5¤/6/g;s/6¤/7/g;s/7¤/8/g;s/8¤/9/g;s/9¤/¤0/g;t a};s/¤/1/g' \
-	support/builders/tow-boot/default.nix
+	's/releaseNumber = "[0-9]*[0-9]/&¤/g;:a {s/0¤/1/g;s/1¤/2/g;s/2¤/3/g;s/3¤/4/g;s/4¤/5/g;s/5¤/6/g;s/6¤/7/g;s/7¤/8/g;s/8¤/9/g;s/9¤/¤0/g;t a};s/¤/1/g' \
+	modules/tow-boot/identity.nix
 
 # Put back the `-pre` suffix
 sed -i -e \
-	's/additionalIdentifier = "";/additionalIdentifier = "-pre";/' \
-	support/builders/tow-boot/default.nix
+	's/releaseIdentifier = "";/releaseIdentifier = "-pre";/' \
+	modules/tow-boot/identity.nix
+
+cat modules/tow-boot/identity.nix
 
 # Commit post-release bump
-git add support/builders/tow-boot/default.nix
+git add modules/tow-boot/identity.nix
 git commit -m "post-release: Bump release number"
