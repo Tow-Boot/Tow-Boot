f870832142689f9ed64655b36eca261c0ca50798
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-06-03 21:30:27 -0400

    tow-boot/smbios: Add fallback sysinfo DT nodes
    These nodes are pre-filled with the Tow-Boot board-specific info. This
    is correct enough as a fallback. We should strive to upstream discrete
    entries to all boards where sysinfo should be filled.
    
    This will require "generic" builds to include different DT entries for
    the different boards in the correct `*-u-boot.dtsi` files.

--- modules/tow-boot/smbios.nix
+++ modules/tow-boot/smbios.nix
@@ -1,5 +1,39 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
+let
+  inherit (pkgs)
+    writeText
+  ;
+
+  # Close enough semantics to C semantics and thus DT strings.
+  escape = builtins.toJSON;
+
+  # Fallback values used if the device description has none.
+  smbios_dtsi = writeText "${config.device.identifier}-smbios.dtsi" ''
+    / {
+      smbios: smbios {
+          compatible = "u-boot,sysinfo-smbios";
+
+          smbios {
+              system {
+                  manufacturer = ${escape config.device.manufacturer};
+                  product = ${escape config.device.name};
+              };
+
+              baseboard {
+                  manufacturer = ${escape config.device.manufacturer};
+                  product = ${escape config.device.name};
+              };
+
+              chassis {
+                  manufacturer = ${escape config.device.manufacturer};
+                  product = ${escape config.device.name};
+              };
+          };
+      };
+    };
+  '';
+in
 {
   Tow-Boot = {
     config = [
@@ -9,5 +43,27 @@
         SYSINFO_SMBIOS = yes;
       })
     ];
+    builder = {
+      # This forcibly adds fallback values for SMBIOS support in device tree files.
+      # This will ensure no "unknown vendor" and such are present, but will instead
+      # force a board-specific value in sysinfo.
+      #
+      # Boards should still add the appropriate DT entries to their `u-boot.dtsi` files.
+      # Especially if the build is shared across different hardware
+      # like for the Pinephone (A64) and Raspberry Pi.
+      #
+      # The fallback values are added only to the "tip-most" *u-boot.dtsi files.
+      postPatch = ''
+        echo " :: Adding build-specific defaults for SMBIOS support..."
+        for f in $(grep -L '#include.*u-boot.dtsi' arch/arm/dts/*u-boot.dtsi); do
+          (
+            echo '#include "smbios.dtsi"'
+            cat "$f"
+          ) > tmp.dts
+          mv tmp.dts "$f"
+        done
+        cp ${smbios_dtsi} arch/arm/dts/smbios.dtsi
+      '';
+    };
   };
 }
