58c1000b34378c42f66f02a6bb685097a4404afc
Nathan Moos <moosingin3space@gmail.com>
2022-06-16 19:33:47 -0400

    pine64-rock64: Add support for ROCK64
    Added without support for display, but with support for starting from
    SPI.
    
    Co-authored-by: Samuel Dionne-Riel <samuel@dionne-riel.com>

---
+++ boards/pine64-rock64/default.nix
@@ -0,0 +1,21 @@
+{
+  device = {
+    manufacturer = "PINE64";
+    name = "ROCK64";
+    identifier = "pine64-rock64";
+    productPageURL = "https://www.pine64.org/rock64/";
+  };
+
+  hardware = {
+    soc = "rockchip-rk3328";
+    SPISize = 16 * 1024 * 1024; # 16 MiB
+    withDisplay = false;
+  };
+
+  Tow-Boot = {
+    defconfig = "rock64-rk3328_defconfig";
+    patches = [
+      ./0001-board-rock64-Enable-booting-from-SPI-flash.patch
+    ];
+  };
+}
