46fda9fbc0acdf0db3f873629d06463cb7217fb1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2022-11-13 18:09:32 -0500

    libreComputer-amlS905xCc: Add support for AML-S905X-CC (Le Potato)

---
+++ boards/libreComputer-amlS905xCc/default.nix
@@ -0,0 +1,21 @@
+{ config, lib, pkgs, ... }:
+
+{
+  device = {
+    manufacturer = "Libre Computer";
+    name = "Le Potato";
+    identifier = "libreComputer-amlS805xAc";
+    productPageURL = "https://libre.computer/products/aml-s905x-cc/";
+  };
+
+  hardware = {
+    soc = "amlogic-s905x";
+  };
+
+  Tow-Boot = {
+    defconfig = "libretech-cc_defconfig";
+    builder.additionalArguments = {
+      FIPDIR = "${pkgs.Tow-Boot.amlogicFirmware}/lepotato";
+    };
+  };
+}
