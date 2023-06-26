6e1fb93b430c9452dc358b5644098e2b3c0296d2
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-06-25 22:14:53 -0400

    raspberryPi-aarch64: Fix build with stock U-Boot

--- boards/raspberryPi-aarch64/default.nix
+++ boards/raspberryPi-aarch64/default.nix
@@ -55,7 +55,9 @@ in
         # 64 MiB; the default unconfigured state is 4 MiB.
         SYS_MALLOC_LEN = freeform ''0x4000000'';
         CMD_POWEROFF = no;
+      })
 
+      (lib.mkIf (!config.Tow-Boot.buildUBoot) (helpers: with helpers; {
         # As far as distro_bootcmd is aware, the raspberry pi can
         # have up to three mmc "devices"
         #   - https://source.denx.de/u-boot/u-boot/-/blob/v2022.07/include/configs/rpi.h#L134-137
@@ -66,7 +68,7 @@ in
         TOW_BOOT_MMC0_NAME = freeform ''"SD (0)"'';
         TOW_BOOT_MMC1_NAME = freeform ''"SD (1)"'';
         TOW_BOOT_MMC2_NAME = freeform ''"SD (2)"'';
-      })
+      }))
     ];
     outputs.firmware = lib.mkIf (config.device.identifier == "raspberryPi-aarch64") (
       pkgs.callPackage (
