4904f2441e63db78d2508e07f2af681f52ec687c
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-07-07 22:30:20 -0400

    support: Add expression to build the sandbox for all known stock U-Boot hashes

---
+++ support/check-all-stock-uboot.nix
@@ -0,0 +1,19 @@
+# This is used to check all stock U-Boot versions don't have unexpected default options regressions.
+# E.g. configuration options not properly gated behind a version.
+# Again, not a guarantee of usefulness, used to keep us more honest.
+let
+  eval = configuration:
+    (import ../. { inherit configuration; }).uBoot-sandbox
+  ;
+
+  # Used to extract known versions.
+  bogus-eval = (eval {});
+
+  evalUBoot = version: eval { Tow-Boot = { buildUBoot = true; uBootVersion = version; }; };
+
+  dropDot = builtins.replaceStrings ["."] ["_"];
+in
+
+builtins.map
+(ver: (evalUBoot ver))
+(builtins.attrNames bogus-eval.config.Tow-Boot.knownHashes.U-Boot)
