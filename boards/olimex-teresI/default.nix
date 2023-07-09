8385fd88aca56036c0579129cbc688350e3daf29
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-07-08 22:08:13 -0400

    boards/olimex-teresI: use crust, as it is now available

--- boards/olimex-teresI/default.nix
+++ boards/olimex-teresI/default.nix
@@ -12,10 +12,5 @@
 
   Tow-Boot = {
     defconfig = "teres_i_defconfig";
-    # Not available in crust
-    # https://github.com/crust-firmware/crust/issues/195
-    builder.additionalArguments = {
-      SCP = null;
-    };
   };
 }
