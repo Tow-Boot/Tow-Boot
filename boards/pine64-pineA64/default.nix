b396cff132fc6f32fb797345136c858dbec76ece
Samuel Dionne-Riel <samuel@dionne-riel.com>
2023-10-07 17:15:19 -0400

    boards/pine64-pineA64: Mark as best-effort

--- boards/pine64-pineA64/default.nix
+++ boards/pine64-pineA64/default.nix
@@ -4,6 +4,7 @@
     name = "A64";
     identifier = "pine64-pineA64";
     productPageURL = "https://www.pine64.org/devices/single-board-computers/pine-a64/";
+    supportLevel = "best-effort";
   };
 
   hardware = {
