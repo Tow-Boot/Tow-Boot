992ab26f42309867688ef709893448184b70fdc9
Nikita Shvets <102628719+shvetsnikita@users.noreply.github.com>
2023-10-07 18:58:23 +0300

    Add support for FriendlyELEC NanoPi K1 Plus

---
+++ boards/friendlyElec-nanoPiK1Plus/default.nix
@@ -0,0 +1,19 @@
+{
+  device = {
+    manufacturer = "FriendlyELEC";
+    name = "NanoPi K1 Plus";
+    identifier = "friendlyElec-nanoPiK1Plus";
+    productPageURL = "https://wiki.friendlyelec.com/wiki/index.php/NanoPi_K1_Plus";
+  };
+
+  hardware = {
+    soc = "allwinner-h5";
+  };
+
+  Tow-Boot = {
+    defconfig = "nanopi_k1_plus_defconfig";
+    patches = [
+      ./0001-nanopi-k1-plus-board-enablement.patch
+    ];
+  };
+}
