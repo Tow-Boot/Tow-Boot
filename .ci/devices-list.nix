53fffbdf79731eb05c8c28e136fc112ba8118e85
997454 <147534832+997454@users.noreply.github.com>
2023-10-10 22:32:15 +0530

    Update devices-list.nix

--- .ci/devices-list.nix
+++ .ci/devices-list.nix
@@ -3,4 +3,10 @@ let
   pkgs = import ../nixpkgs.nix {};
   release-tools = import ../support/nix/release-tools.nix { inherit pkgs; };
 in
-  release-tools.allDevices
+  release-tools.allDevices in clothas and nl anmsjsns ma shssvsbs skms dns js sn djs dbs ka skskks 
+fsvsjs ms sjskks rhe k ehd is dh j(/?₹ ;,,, ghajs 
+gshgsg
+    :)!:₹
+  shs
+vvsvvs 
+ 
