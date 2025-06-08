ca57ddc5f6255bb35585e60dccb7ba06dd5ea1ee
erdnaxe <erdnaxe@crans.org>
2025-06-08 20:25:29 +0200

    Add NanoPi Neo4

---
+++ boards/friendlyElec-nanoPiNeo4/default.nix
@@ -0,0 +1,17 @@
+{
+  device = {
+    manufacturer = "FriendlyELEC";
+    name = "NanoPi Neo4";
+    identifier = "friendlyElec-nanoPiNeo4";
+    productPageURL = "https://wiki.friendlyelec.com/wiki/index.php/NanoPi_NEO4";
+    supportLevel = "best-effort";
+  };
+
+  hardware = {
+    soc = "rockchip-rk3399";
+  };
+
+  Tow-Boot = {
+    defconfig = "nanopi-neo4-rk3399_defconfig";
+  };
+}
