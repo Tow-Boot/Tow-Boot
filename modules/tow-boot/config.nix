5ba60d4105603587c97fdf16e43b5b0ac04f1146
Petr Hodina <petr.hodina@protonmail.com>
2026-01-09 17:11:15 +0000

    tow-boot: Add Android boot image generation for androidboot variant
    Generate Android boot images (.img) for the androidboot variant instead of
    standard disk images. Follows official U-Boot Qualcomm documentation by
    gzipping u-boot-nodtb.bin, concatenating all SDM845 DTBs for runtime device
    detection, and packaging with mkbootimg.
    
    Also make boot configuration options conditional to support both distro_bootcmd
    and bootstd approaches, and add extra-utils for xxd dependency.
    
    Signed-off-by: Petr Hodina <petr.hodina@protonmail.com>

--- modules/tow-boot/config.nix
+++ modules/tow-boot/config.nix
@@ -148,11 +148,11 @@ in
       # For now, it's outright disabled, we will need to re-evaluate our
       # infra to work with bootstd, but only after a larger proportion of
       # the devices default to `bootstd`.
-      BOOTSTD = no;
-      BOOTSTD_DEFAULTS = no;
-      DISTRO_DEFAULTS = yes;
-      USE_BOOTCOMMAND = yes;
-      BOOTCOMMAND = freeform ''"run distro_bootcmd"'';
+      BOOTSTD = option no;
+      BOOTSTD_DEFAULTS = option no;
+      DISTRO_DEFAULTS = option yes;
+      USE_BOOTCOMMAND = option yes;
+      BOOTCOMMAND = option (freeform ''"run distro_bootcmd"'');
     })
 
     # Logo handling
